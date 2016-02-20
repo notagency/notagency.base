4c4e49673b4dd346e9dd908c8dee222b0b5d61c5
Dmitry Savchenkov <bitrixdev@gmail.com>
2016-02-20 22:48:15 +0300

    added catalog.

---
+++ install/components/notagency/catalog.list/.description.php
@@ -0,0 +1,15 @@
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+    'NAME' => basename(__DIR__),
+    'DESCRIPTION' => '',
+    'ICON' => '/images/news_list.gif',
+    'SORT' => 20,
+    'CACHE_PATH' => 'Y',
+    'PATH' => array(
+      	'ID' => basename(dirname(__DIR__)),
+    ),
+);
+
+?>

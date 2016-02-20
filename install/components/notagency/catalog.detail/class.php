c578be153e29a0df71301d7478c907551b98f04b
Dmitry Savchenkov <bitrixdev@gmail.com>
2016-02-20 23:12:44 +0300

    added catalog.detail

---
+++ install/components/notagency/catalog.detail/class.php
@@ -0,0 +1,38 @@
+<?
+namespace Notagency\Components;
+
+\CBitrixComponent::includeComponentClass('notagency:materials.detail');
+
+class CatalogDetail extends MaterialsDetail
+{
+    protected $needModules = [
+        'catalog'
+    ];
+
+    protected function processElement($element)
+    {
+        $element = $this->processAmount($element);
+        $element = $this->processPrice($element);
+        return $element;
+    }
+
+    protected function processAmount($element)
+    {
+        $product = \CCatalogProduct::GetByID($element['ID']);
+        $element['QUANTITY'] = $product['QUANTITY'];
+        return $element;
+    }
+
+    protected function processPrice($element)
+    {
+        $element['CATALOG_DETAILS'] = \CCatalogProduct::GetOptimalPrice($element['ID']);
+        $element['PRICE_FORMATTED'] = $this->formatPrice($element['CATALOG_DETAILS']['PRICE']['PRICE']);
+        return $element;
+    }
+
+    protected function formatPrice($price)
+    {
+        $price = \CCurrencyLang::CurrencyFormat($price, 'RUB', false);
+        return $price;
+    }
+}

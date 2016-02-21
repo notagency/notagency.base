314816f5971fa7c9ea6f0db0d6e4caab6f5653d2
Dmitry Savchenkov <bitrixdev@gmail.com>
2016-02-21 03:12:26 +0300

    jsonformatter trait added

---
+++ lib/traits/jsonformatter.php
@@ -0,0 +1,18 @@
+<?
+namespace Notagency\Base\Traits;
+
+trait JsonFormatter
+{
+    protected function applyJsonHeaders()
+    {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Content-type: application/json');
+    }
+
+    public function showResult()
+    {
+        $this->applyJsonHeaders();
+        echo json_encode($this->arResult, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+}

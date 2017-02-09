6913c15bcbc5aa148a715a6fcd1aa2f7b270c06f
Dmitry Savchenkov <ds@notagency.ru>
2017-02-09 21:41:55 +0300

    moved module to outer repo, updated readme.md and composer.json dependencies

--- install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$arModuleVersion = [
-    'VERSION' => '1.0.2.91',
-    'VERSION_DATE' => '2017-01-25'
-];
--- lang/ru/lib/componentsbase.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$MESS['NIK_COMPONENT_ERROR_OCCURED'] = 'Произошла внутренняя ошибка';
-$MESS['NIK_FAILED_INCLUDE_MODULE'] = 'Не удалось подключить модуль #MODULE#';

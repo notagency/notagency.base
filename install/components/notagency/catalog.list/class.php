ef0a8f943dfdf4456e12cda8b13c20984a826a15
Andrey Eremichev <30293802+paradoxst108@users.noreply.github.com>
2018-07-13 14:59:00 +0300

    Update class.php

--- install/components/notagency/catalog.list/class.php
+++ install/components/notagency/catalog.list/class.php
@@ -22,19 +22,29 @@ class CatalogList extends MaterialsList
     {
         $product = \CCatalogProduct::GetByID($element['ID']);
         $element['QUANTITY'] = $product['QUANTITY'];
+
+        if (empty($product['MEASURE'])) {
+            $arDefaultMeasure = \CIBlockPriceTools::GetDefaultMeasure();
+            $element['CATALOG_MEASURE_NAME'] = $arDefaultMeasure['SYMBOL_RUS'];
+        } else {
+            $rsMeasures = \CCatalogMeasure::getList(false, array('ID' => $product['MEASURE']));
+            if ($arMeasure = $rsMeasures->GetNext()) {
+                $element['CATALOG_MEASURE_NAME'] = $arMeasure['SYMBOL_RUS'];
+            }
+        }
         return $element;
     }
 
     protected function processPrice($element)
     {
         $element['CATALOG_DETAILS'] = \CCatalogProduct::GetOptimalPrice($element['ID']);
-        $element['PRICE_FORMATTED'] = $this->formatPrice($element['CATALOG_DETAILS']['PRICE']['PRICE']);
+        $element['PRICE_FORMATTED'] = $this->formatPrice($element['CATALOG_DETAILS']['PRICE']['PRICE'], $element['CATALOG_DETAILS']['PRICE']['CURRENCY'], true);
         return $element;
     }
 
-    protected function formatPrice($price)
+    protected function formatPrice($price, $currency = 'RUB', $useTemplate = false)
     {
-        $price = \CCurrencyLang::CurrencyFormat($price, 'RUB', false);
+        $price = \CCurrencyLang::CurrencyFormat($price, $currency, $useTemplate);
         return $price;
     }
 }
